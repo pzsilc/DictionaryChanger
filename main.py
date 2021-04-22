f1e0ee03ce1f1c0d659dadbd53f1339fb80b545c
pzsilc <patryk.znamirowski@silcare.com>
2021-04-22 10:46:50 +0200

    Add comments

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ init()
 
 
 class Product:
-    def __init__(self, headers, attrs):
+    def __init__(self, headers, attrs): #product has only 2 props - code and attrs (attrs are all values of next dictionaires)
         self.code = attrs[0]
         self.attrs = {}
         for index, header in enumerate(headers):
@@ -22,12 +22,12 @@ class Product:
 class Changer:
 
     def __init__(self):
-        self.__guid = None
-        self.__session = ''
-        with open('config.json') as config:
+        self.__guid = None #key for webapi of symfonia
+        self.__session = '' #session token
+        with open('config.json') as config: #load guid
             c = json.loads(config.read())
             self.__guid = c['guid']
-        self.__options = requests.get('http://192.168.0.234/dictionaires?name=')
+        self.__options = requests.get('http://192.168.0.234/dictionaires?name=') #load all dictionaires
         if self.__options.status_code > 400:
             raise Exception('Unable to load options from db')
         else:
@@ -35,11 +35,11 @@ class Changer:
 
 
     def get_id_from_value(self, dimension, value):
-        if isinstance(value, int) and value < 10: value = '0' + str(value)
+        if isinstance(value, int) and value < 10: value = '0' + str(value) #i assume each of numeric values under 10 has to contains '0' char front of
         for i in self.__options:
-            if i['dict_name'] == str(dimension) and i['val_name'] == str(value):
-                    return i['val_id']
-        return None
+            if i['dict_name'] == str(dimension) and i['val_name'] == str(value): #searching suitable dictionaire base on data in parameters
+                    return i['val_id'] #return dictionaire-value-id
+        return None #not found
     
 
     def login(self):
@@ -63,10 +63,10 @@ class Changer:
 
 
     def run(self, products):
-        errors = []
+        errors = [] #list of products didn't pass
         for product in products:
             print(Style.RESET_ALL)
-            data = []
+            data = [] #body of request to api
             for key in product.attrs:
                 data.append({
                     "Code": key,
@@ -81,11 +81,11 @@ class Changer:
                 }
             )
             if res.status_code < 400: 
-                print(f'{product.code} został zaktualizowany')
+                print(f'{product.code} został zaktualizowany') #no errors
             else:
                 errors.append(product)    
 
-        if len(errors):
+        if len(errors): #listing errors (if any exists)
             print(Fore.RED + 'Something went wrong with:')
             for index, product in enumerate(errors):
                 print(index+1, product.code)
@@ -98,22 +98,22 @@ class App(QWidget):
     def __init__(self):
         super().__init__()
         self.title = 'Changer'
-        self.left = 300
+        self.left = 300 #properties of window
         self.top = 300
         self.width = 200
         self.height = 100
-        self.filename = None
-        self.products = list()
+        self.filename = None #path to choosed file
+        self.products = list() #list of Product instances
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.attach_btn = QPushButton('Attach file (xlsx)', self)
+        self.attach_btn = QPushButton('Attach file (xlsx)', self) #button 1
         self.attach_btn.clicked.connect(self.attach)
         self.attach_btn.resize(160, 30)
         self.attach_btn.move(20, 20)
-        self.submit_btn = QPushButton('Export', self)
+        self.submit_btn = QPushButton('Export', self) #button 2
         self.submit_btn.clicked.connect(self.submit)
         self.submit_btn.resize(160, 30)
         self.submit_btn.move(20, 60)
@@ -122,17 +122,17 @@ class App(QWidget):
     def attach(self):
         self.open_file()
         if self.filename:
-            xlsx = ExcelFile(self.filename)      
-            df = xlsx.parse(xlsx.sheet_names[0])
-            df = df.to_dict()
-            if next(iter(df)) != 'Kod':
+            xlsx = ExcelFile(self.filename) #parse choosed 
+            df = xlsx.parse(xlsx.sheet_names[0]) #file
+            df = df.to_dict() #to py dict
+            if next(iter(df)) != 'Kod': #uncorrect format of file
                 print(Fore.RED + 'Dane są w nieodpowiednim formacie')
                 self.products.clear()
             else:
                 headers = list(df.keys())
                 for i in df['Kod']:
                     attrs = [ df[key][i] for key in df ]
-                    self.products.append(Product(headers, attrs))
+                    self.products.append(Product(headers, attrs)) #save data from file to list of products
 
     def open_file(self):
         options = QFileDialog.Options()
@@ -151,6 +151,7 @@ class App(QWidget):
 
 
 
+#start app
 if __name__ == '__main__':
     print(Style.RESET_ALL)
     app = QApplication(sys.argv)
